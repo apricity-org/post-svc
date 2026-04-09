acc2e94d87c3628b8aaa67437ddb0cfa4206488c
Michael-Safwat <mikesafwat34@gmail.com>
2026-04-09 13:55:40 +0200

    initial commit

---
+++ src/main/java/com/haze/post_svc/PostSvcApplication.java
@@ -0,0 +1,13 @@
+package com.haze.post_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haze/post_svc/PostSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haze.post_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
